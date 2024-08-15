ec0586ba3ab4cf981979dd65b20ca38ddc22c236
Zouhair <darkzouhair@gmail.com>
2024-08-15 19:53:20 +0000

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
 """
-Web cache and tracker
+web cache and tracker
 """
 import requests
 import redis
 from functools import wraps
-from typing import Callable
 
 store = redis.Redis()
 
-def count_url_access(method: Callable) -> Callable:
-    """ Decorator counting how many times a URL is accessed """
+
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def wrapper(url: str) -> str:
-        store.incr(f"count:{url}")
-        cached_key = f"cached:{url}"
+    def wrapper(url):
+        cached_key = "cached:" + url
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
         html = method(url)
-        store.setex(cached_key, 10, html)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
         return html
     return wrapper
+
 
 @count_url_access
 def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
